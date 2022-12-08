2f726ade639c4626c7a47413438f31b7e75976b5
Sebastian <sebwacek@gmail.com>
2022-12-08 21:23:57 +0100

    Login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,48 +7,56 @@ export default function LoginScreen({navigation}){
         <Image
             source={require("./logo.png")}
             style={{width: 150, height: 150, borderRadius: 100}}
+            alt={"Alt"}
         />
-        <Box safeArea p="2" py="8" w="100%" maxW="290">
+        <Box safeArea mb="20" p="2" py="8" w="90%" >
             <Heading size="lg"  fontWeight="600" color="coolGray.800" _dark={{
                 color: "warmGray.50"
             }}>
             </Heading>
             <Box>
-            <VStack space={10} mt="10">
+            <VStack space={8} mt="1">
                 <FormControl>
-                    <FormControl.Label>Email ID</FormControl.Label>
-                    <Input />
+                    <FormControl.Label>
+                        <Text style = {{fontSize: 25, fontWeight: "bold"}}>
+                        Login
+                        </Text>
+                    </FormControl.Label>
+                    <Input w="100%"  backgroundColor="white"/>
                 </FormControl>
                 <FormControl>
-                    <FormControl.Label>Password</FormControl.Label>
-                    <Input type="password" />
-                    <Link _text={{
-                        fontSize: "xs",
-                        fontWeight: "500",
-                        color: "indigo.500"
-                    }} alignSelf="flex-end" mt="1">
-                        Forget Password?
-                    </Link>
+                    <FormControl.Label>
+                        <Text style = {{fontSize: 25, fontWeight: "bold"}}>
+                       Hasło
+                        </Text>
+                    </FormControl.Label>
+                    <Input type="password" backgroundColor="white"/>
                 </FormControl>
-                <Button color="#63C5C5" width="150"  ml="60">
-                    Sign in
+                <Center>
+                <Button borderRadius="10" mt="3" color="#63C5C5" width="150">
+                    Zaloguj
                 </Button>
-                <HStack mt="6" justifyContent="center">
-                    <Text fontSize="sm" color="coolGray.600" _dark={{
-                        color: "warmGray.200"
-                    }}>
-                        I'm a new user.{" "}
-                    </Text>
-                    <Link _text={{
+                </Center>
+                <VStack mb="6" alignItems="center">
+                     <Link _text={{
                         color: "indigo.500",
-                        fontWeight: "medium",
+                        fontWeight: "small",
                         fontSize: "sm"
                     }} href="#">
-                        Sign Up
+                        Załóż konto
                     </Link>
-                </HStack>
+                </VStack>
             </VStack>
             </Box>
         </Box>
+        <VStack mt="13" mb="-10" alignItems="center">
+            <Link _text={{
+                color: "indigo.500",
+                fontWeight: "small",
+                fontSize: "sm"
+            }} href="#">
+                Nie pamiętasz hasła?
+            </Link>
+        </VStack>
     </Center>
 };
